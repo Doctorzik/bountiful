7e9d03ae0ce89a4e83758b3038504498b964e5ca
doctorzik <ezekieljeremiah92@gmail.com>
2023-07-18 01:14:05 +0100

    iohiio

--- scripts/fruit.js
+++ scripts/fruit.js
@@ -7,6 +7,62 @@ async function apiFetch() {
       const data = await response.json();
       console.log(data);
       populateSelect(data);
+
+      document
+        .getElementById("special-drink")
+        .addEventListener("submit", function (event) {
+          event.preventDefault();
+          const f1 = document.getElementById("fruit1").value;
+          const f2 = document.getElementById("fruit2").value;
+          const f3 = document.getElementById("fruit3").value;
+          const firstName = document.getElementById("fname").value;
+          const email = document.getElementById("email").value;
+          const phone = document.getElementById("phone").value;
+          const instructions = document.getElementById("instructions").value;
+          console.log(f1);
+          console.log(f2);
+          console.log(f3);
+          console.log(firstName);
+          console.log(email);
+          console.log(phone);
+          console.log(instructions);
+
+          const selectedFruits = [f1, f2, f3];
+          let totalCarbs = 0;
+          let totalProtein = 0;
+          let totalFat = 0;
+          let totalSugar = 0;
+          let totalCalories = 0;
+          selectedFruits.forEach((selectedFruit) => {
+            const fruit = data.find((fruit) => fruit.name === selectedFruit);
+            if (fruit) {
+              totalCarbs += fruit.nutritions.carbohydrates;
+              totalProtein += fruit.nutritions.protein;
+              totalFat += fruit.nutritions.fat;
+              totalSugar += fruit.nutritions.sugar;
+              totalCalories += fruit.nutritions.calories;
+            }
+          });
+          const output = `
+            <h2>Your Order:</h2>
+            <p><strong>First Name:</strong> ${firstName}</p>
+            <p><strong>Email:</strong> ${email}</p>
+            <p><strong>Phone Number:</strong> ${phone}</p>
+            <p><strong>Selected Fruits:</strong> ${selectedFruits.join(
+              ", "
+            )}</p>
+            <p><strong>Special Instructions:</strong> ${instructions}</p>
+            <h3>Nutritional Information:</h3>
+            <p><strong>Total Carbohydrates:</strong> ${totalCarbs} g</p>
+            <p><strong>Total Protein:</strong> ${totalProtein} g</p>
+            <p><strong>Total Fat:</strong> ${totalFat} g</p>
+            <p><strong>Total Sugar:</strong> ${totalSugar} g</p>
+            <p><strong>Total Calories:</strong> ${totalCalories} cal</p>
+          `;
+
+          // Display the output
+          document.getElementById("outputArea").innerHTML = output;
+        });
     } else {
       throw Error(await response.text());
     }
@@ -22,11 +78,11 @@ const populateSelect = (items) => {
     let option2 = document.createElement("option");
     let option3 = document.createElement("option");
 
-    option.innerHTML = element.name;
+    option.textContent = element.name;
 
-    option2.innerHTML = element.name;
+    option2.textContent = element.name;
 
-    option3.innerHTML = element.name;
+    option3.textContent = element.name;
 
     document.getElementById("fruit1").append(option);
     document.getElementById("fruit2").append(option2);
